b4f71fa09d0934b93998c5f2c850705d99a4af1a
Parva <parvadesai404@gmail.com>
2023-10-18 14:23:00 +0530

    feat: Added an example for chain movement
    For understanding OnComplete(), Sequence(), Append().

---
+++ Assets/Scripts/ChainMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class ChainMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] shapes;
+    
+    void Start()
+    {
+        shapes[0].DOMoveX(7, Random.Range(1f, 2f)).OnComplete(() =>
+        {
+            shapes[1].DOMoveX(7, Random.Range(1f, 2f)).OnComplete(() =>
+            {
+                shapes[2].DOMoveX(7, Random.Range(1f, 2f)).OnComplete(() =>
+                {
+                    foreach (var shape in shapes)
+                    {
+                        shape.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce);
+                    }
+                });
+            });
+        });
+
+        // var sequence = DOTween.Sequence();
+        //
+        // foreach (var shape in shapes)
+        // {
+        //     sequence.Append(shape.DOMoveX(7, Random.Range(1f, 2f)));
+        // }
+        //
+        // sequence.OnComplete(() =>
+        // {
+        //     foreach (var shape in shapes)
+        //     {
+        //         shape.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce);
+        //     }
+        // });
+        
+        // AsyncMove();
+        
+        // Tasks();
+    }
+
+    async void AsyncMove()
+    {
+        foreach (var shape in shapes)
+        {
+            await shape.DOMoveX(7, Random.Range(1f, 2f)).SetEase(Ease.InOutQuad).AsyncWaitForCompletion();
+        }
+    }
+
+    async void Tasks()
+    {
+        var tasks = new List<Task>();
+        
+        foreach (var shape in shapes)
+        {
+            tasks.Add(shape.DOMoveX(7, Random.Range(1f, 2f)).SetEase(Ease.InOutQuad).AsyncWaitForCompletion());
+        }
+
+        await Task.WhenAll(tasks);
+
+        foreach (var shape in shapes)
+            shape.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce);
+    }
+}
